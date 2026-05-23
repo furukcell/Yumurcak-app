cae0c20a6d623726d48191d60323a924c62fef34
furukcell <kurtulusfaruk94@gmail.com>
2026-05-23 12:53:07 +0300

    Create id.ts

---
+++ src/utils/id.ts
@@ -0,0 +1,12 @@
+export const generateId = (): string => {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < 20; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+};
+
+export const formatYYYYMMDD = (date: Date = new Date()): string => {
+  return date.toISOString().split('T')[0];
+};
