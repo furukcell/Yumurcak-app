f827cd12e4719d3635156bfad65e847ad256f199
furukcell <kurtulusfaruk94@gmail.com>
2026-06-18 08:42:35 +0300

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -6,7 +6,6 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
-// Expo'da .env değişkenleri otomatik yüklenir (EXPO_PUBLIC_ prefix)
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY || '',
   authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN || '',
