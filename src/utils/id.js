d67ae403236e4c3f75b7d91a05ac8b9c1b11b0c0
furukcell <kurtulusfaruk94@gmail.com>
2026-06-19 21:45:12 +0300

    Fix JS syntax in id utils

--- src/utils/id.js
+++ src/utils/id.js
@@ -1,12 +1,14 @@
-export const generateId = (): string => {
+export const generateId = () => {
   const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let result = '';
+
   for (let i = 0; i < 20; i++) {
     result += chars.charAt(Math.floor(Math.random() * chars.length));
   }
+
   return result;
 };
 
-export const formatYYYYMMDD = (date: Date = new Date()): string => {
+export const formatYYYYMMDD = (date = new Date()) => {
   return date.toISOString().split('T')[0];
 };
