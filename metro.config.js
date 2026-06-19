fbafbd3dafc1b79cc5f4171b7dd281a64f40dc7b
furukcell <kurtulusfaruk94@gmail.com>
2026-06-19 15:36:47 +0300

    Create metro.config.js

---
+++ metro.config.js
@@ -0,0 +1,12 @@
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+config.resolver.sourceExts = [
+  ...config.resolver.sourceExts,
+  'cjs',
+];
+
+config.resolver.unstable_enablePackageExports = false;
+
+module.exports = config;
