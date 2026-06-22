4ec083f37070d6312ae60e9351a93da83d79f8ac
furukcell <kurtulusfaruk94@gmail.com>
2026-06-22 11:22:53 +0300

    Update AuthStack.js

--- src/navigation/AuthStack.js
+++ src/navigation/AuthStack.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from '../screens/auth/LoginScreen';
+import LegalDocumentsScreen from '../screens/legal/LegalDocumentsScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -8,6 +9,7 @@ export default function AuthStack() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="LegalDocuments" component={LegalDocumentsScreen} />
     </Stack.Navigator>
   );
 }
