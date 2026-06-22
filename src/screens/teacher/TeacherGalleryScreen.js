9525c1d2842a4797d5bed1ec0dfca890c010ffab
furukcell <kurtulusfaruk94@gmail.com>
2026-06-22 10:58:26 +0300

    Add teacher gallery wrapper

---
+++ src/screens/teacher/TeacherGalleryScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import GalleryScreenBase from '../shared/GalleryScreenBase';
+
+function TeacherGalleryScreen(props) {
+  return <GalleryScreenBase mode="teacher" navigation={props.navigation} />;
+}
+
+export default TeacherGalleryScreen;
