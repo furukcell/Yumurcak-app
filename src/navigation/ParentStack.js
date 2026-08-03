2cf1e5df262e34b0b3380fd9fe5c96106b6d3eeb
furukcell <kurtulusfaruk94@gmail.com>
2026-08-03 11:14:23 +0300

    Update ParentStack.js

--- src/navigation/ParentStack.js
+++ src/navigation/ParentStack.js
@@ -13,6 +13,7 @@ import ParentProfileScreen from '../screens/parent/ParentProfileScreen';
 import ParentAboutScreen from '../screens/parent/ParentAboutScreen';
 import ParentSupportScreen from '../screens/parent/ParentSupportScreen';
 import ParentMealsScreen from '../screens/parent/ParentMealsScreen';
+import ParentBulletinScreen from '../screens/parent/ParentBulletinScreen';
 import ParentEventsScreen from '../screens/parent/ParentEventsScreen';
 import ParentAttendanceScreen from '../screens/parent/ParentAttendanceScreen';
 import ParentDevelopmentScreen from '../screens/parent/ParentDevelopmentScreen';
@@ -80,6 +81,7 @@ export default function ParentStack() {
         <Stack.Screen name="ParentAbout" component={ParentAboutScreen} />
         <Stack.Screen name="ParentSupport" component={ParentSupportScreen} />
         <Stack.Screen name="ParentMeals" component={ParentMealsScreen} />
+        <Stack.Screen name="ParentBulletin" component={ParentBulletinScreen} />
         <Stack.Screen name="ParentEvents" component={ParentEventsScreen} />
         <Stack.Screen name="ParentAttendance" component={ParentAttendanceScreen} />
         <Stack.Screen name="ParentDevelopment" component={ParentDevelopmentScreen} />
