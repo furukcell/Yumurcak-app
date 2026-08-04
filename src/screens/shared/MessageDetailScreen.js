9c5374d69fa5b42b0b0f1a1835b04b2b8cb7b8a9
furukcell <kurtulusfaruk94@gmail.com>
2026-08-04 12:21:55 +0300

    Update MessageDetailScreen.js

--- src/screens/shared/MessageDetailScreen.js
+++ src/screens/shared/MessageDetailScreen.js
@@ -295,7 +295,7 @@ export default function MessageDetailScreen() {
     <View style={[styles.safeArea, { paddingTop: insets.top }]}>
       <KeyboardAvoidingView
       style={styles.flex}
-      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       keyboardVerticalOffset={0}
     >
         <View style={styles.header}>
