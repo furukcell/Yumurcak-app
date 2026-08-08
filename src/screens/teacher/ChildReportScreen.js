c80bd726eaef1c66b72384d9a51bd0718e9da531
furukcell <kurtulusfaruk94@gmail.com>
2026-08-08 08:29:34 +0300

    Update ChildReportScreen.js

--- src/screens/teacher/ChildReportScreen.js
+++ src/screens/teacher/ChildReportScreen.js
@@ -171,7 +171,7 @@ export default function ChildReportScreen() {
 
       <KeyboardAvoidingView
         style={styles.screen}
-        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
         keyboardVerticalOffset={Platform.OS === 'ios' ? 90 : 0}
       >
         <ScrollView
