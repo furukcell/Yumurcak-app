d98026c83b49b01748276c4b600717652b1bd27e
furukcell <kurtulusfaruk94@gmail.com>
2026-08-08 08:41:18 +0300

    Update AdminMonthlyMealScreen.js

--- src/screens/admin/AdminMonthlyMealScreen.js
+++ src/screens/admin/AdminMonthlyMealScreen.js
@@ -371,7 +371,7 @@ export default function AdminMonthlyMealScreen({ navigation }) {
         <Modal visible={!!selectedDay} transparent animationType="slide" onRequestClose={() => setSelectedDateKey('')}>
           <KeyboardAvoidingView
             style={styles.modalBackdrop}
-            behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
             keyboardVerticalOffset={Platform.OS === 'ios' ? 40 : 0}
           >
             <View style={styles.modalSheet}>
