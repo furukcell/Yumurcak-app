38dc4cf119266d7f4827394acc8d0ab5965ad1c4
furukcell <kurtulusfaruk94@gmail.com>
2026-08-08 08:46:02 +0300

    Update PollManagementScreen.js

--- src/screens/admin/PollManagementScreen.js
+++ src/screens/admin/PollManagementScreen.js
@@ -278,7 +278,7 @@ export default function PollManagementScreen() {
       />
      <KeyboardAvoidingView
         style={{ flex: 1 }}
-        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
         keyboardVerticalOffset={Platform.OS === 'ios' ? 90 : 0}
       >
       <ScrollView style={styles.screen} contentContainerStyle={styles.content} keyboardShouldPersistTaps="handled">
