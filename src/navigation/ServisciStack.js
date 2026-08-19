a648bf7a9763f47f8de12521ebed6a3d011b98c8
furukcell <kurtulusfaruk94@gmail.com>
2026-08-19 08:30:00 +0300

    Update ServisciStack.js

--- src/navigation/ServisciStack.js
+++ src/navigation/ServisciStack.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import ServisciDashboardScreen from '../screens/servisci/ServisciDashboardScreen';
+import ServisciRouteScreen from '../screens/servisci/ServisciRouteScreen';
 import LegalDocumentsScreen from '../screens/legal/LegalDocumentsScreen';
 
 const Stack = createNativeStackNavigator();
@@ -10,6 +11,7 @@ export default function ServisciStack() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="ServisciDashboard" component={ServisciDashboardScreen} />
+      <Stack.Screen name="ServisciRoute" component={ServisciRouteScreen} />
       <Stack.Screen name="LegalDocuments" component={LegalDocumentsScreen} />
     </Stack.Navigator>
   );
