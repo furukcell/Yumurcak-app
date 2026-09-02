6761be294d148f07649c96f4f484489fa504fc2c
furukcell <kurtulusfaruk94@gmail.com>
2026-09-02 10:05:17 +0300

    Update SuperAdminStack.js

--- src/navigation/SuperAdminStack.js
+++ src/navigation/SuperAdminStack.js
@@ -10,6 +10,7 @@ import SuperAdminMenuScreen from '../screens/superadmin/SuperAdminMenuScreen';
 import SuperAdminDashboardScreen from '../screens/superadmin/SuperAdminDashboardScreen';
 import SuperAdminKresDetailScreen from '../screens/superadmin/SuperAdminKresDetailScreen';
 import SuperAdminKresCreateScreen from '../screens/superadmin/SuperAdminKresCreateScreen';
+import SuperAdminKresBulkOnboardingScreen from '../screens/superadmin/SuperAdminKresBulkOnboardingScreen';
 import SuperAdminIndexMigrationScreen from '../screens/superadmin/SuperAdminIndexMigrationScreen';
 import SuperAdminSupportScreen from '../screens/superadmin/SuperAdminSupportScreen';
 import SuperAdminSubscriptionsScreen from '../screens/superadmin/SuperAdminSubscriptionsScreen';
@@ -29,6 +30,7 @@ export default function SuperAdminStack() {
       <Stack.Screen name="SuperAdminDashboard" component={SuperAdminDashboardScreen} />
       <Stack.Screen name="SuperAdminKresDetail" component={SuperAdminKresDetailScreen} />
       <Stack.Screen name="SuperAdminKresCreate" component={SuperAdminKresCreateScreen} />
+      <Stack.Screen name="SuperAdminKresBulkOnboarding" component={SuperAdminKresBulkOnboardingScreen} />
       <Stack.Screen name="SuperAdminIndexMigration" component={SuperAdminIndexMigrationScreen} />
       <Stack.Screen name="SuperAdminSupport" component={SuperAdminSupportScreen} />
       <Stack.Screen name="SuperAdminSubscriptions" component={SuperAdminSubscriptionsScreen} />
